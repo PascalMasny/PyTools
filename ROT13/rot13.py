22d1fc16ecd027a3735dc63cf178ffdde476d1b7
Passi <62151638+PascalMasny@users.noreply.github.com>
2021-05-25 19:04:30 +0200

    Initial Commit

---
+++ ROT13/rot13.py
@@ -0,0 +1,19 @@
+import string
+
+upper = list(string.ascii_uppercase)
+lower = list(string.ascii_lowercase)
+
+def rot13(msg):
+    result = ""
+    
+    for character in msg:
+        if character in upper:
+            result += upper[(upper.index(character) + 23) % 26]
+        elif character in lower:
+            result += lower[(lower.index(character) + 23) % 26]
+        else:
+            result += character
+        
+    return result
+
+print(rot13("Hello World"))
