f896239d3e4905cb2d944436496dd36e647bc1c4
Passi <62151638+PascalMasny@users.noreply.github.com>
2021-06-01 17:59:32 +0200

    Initial Commit

---
+++ Gold price/gold_price.py
@@ -0,0 +1,28 @@
+# importing libraries
+from bs4 import BeautifulSoup as BS
+import requests
+  
+  
+# method to get the price of gold
+def get_price(url):
+      
+    # getting the request from url 
+    data = requests.get(url)
+  
+    # converting the text 
+    soup = BS(data.text, 'html.parser')
+  
+    # finding metha info for the current price
+    ans = soup.find("div", class_ = "BNeawe s3v9rd AP7Wnd").text
+      
+    # returnng the price
+    return ans
+   
+# url of the gold price
+url = "https://www.google.com/search?q=gold+price"
+  
+# calling the get_price method
+ans = get_price(url)
+  
+# printing the ans
+print(ans)
